af77c2d8c6da610e99cd681c0999b982924bb61c
gerryoncode <gerryoncode@gmail.com>
2023-02-16 05:10:04 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-
-int main (void)
+/**
+ * main - Entry point
+ * puts - prints text on screen
+ * Return: 0 if succesful
+ * Description: Outputs a string using puts
+ */
+int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
-	return(0);
+	return (0);
 }
