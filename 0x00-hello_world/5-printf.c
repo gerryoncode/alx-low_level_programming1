5485d29f7b049eab5c505c6ba9035e1d8c8f9b78
gerryoncode <gerryoncode@gmail.com>
2023-02-16 05:14:05 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * printf - prnts formatted text
+ * Return: 0 value for success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
