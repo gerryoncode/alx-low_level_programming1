e5cf9d62d954927ba4a84f78f6f775f50191d66a
gerryoncode <gerryoncode@gmail.com>
2023-02-16 06:14:56 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * stderr - Standard error stream
+ * Return: Always zero
+ */
+int main(void)
+{
+	fprintf(stderr, "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
