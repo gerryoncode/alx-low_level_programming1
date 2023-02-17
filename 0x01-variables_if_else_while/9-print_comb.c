38da7b26e5e24e939d915c8c3a914991ec0a830d
gerryoncode <gerryoncode@gmail.com>
2023-02-17 07:25:18 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,15 @@
+#include <stdin.h>
+/**
+ * main - Entry point
+ * Return: Alway 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 9)
+	{
+		putchar((c % 10) + ',');
+	}
+	putchar((9 % 10));
+}
