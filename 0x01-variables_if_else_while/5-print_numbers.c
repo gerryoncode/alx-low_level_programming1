d3b8c2fd14106fb472b0c2d63d51342e93078a56
gerryoncode <gerryoncode@gmail.com>
2023-02-17 05:07:38 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c = 0;
+	while (c < = 9 )
+	{
+		printf("%d" , c++);
+	}
+	putchar('\n');
+	return 0;
+}
