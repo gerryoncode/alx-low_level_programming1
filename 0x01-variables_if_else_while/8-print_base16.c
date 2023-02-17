216ed9bd24e02db39180f96deddf58e40f6ac7c7
gerryoncode <gerryoncode@gmail.com>
2023-02-17 07:08:27 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry pointof the function
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c <= 10)
+	{
+		putchar((c % 10) + '0');
+	}
+	while (n <= 'f')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
