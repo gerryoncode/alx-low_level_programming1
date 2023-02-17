a171dda27cfa170030d46579129d410b8ae1f1c5
gerryoncode <gerryoncode@gmail.com>
2023-02-17 02:35:18 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints lowecas ealphabets
+ * putchar - Prints a single character at a time.
+ * Return: 0 for success
+ * /
+
+int main(void)
+{
+	int c = 'a';
+
+	while(c <='z')
+	{
+		putchar(c++);
+	}
+	return 0;
+}
