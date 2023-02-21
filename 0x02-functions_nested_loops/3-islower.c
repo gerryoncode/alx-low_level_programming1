81bb00325cc75432af22adfb4bb565e32078e31c
gerryoncode <gerryoncode@gmail.com>
2023-02-21 11:40:36 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+int "main.h"
+/**
+ * _islower - Checks if the alphabets are lowercase
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
