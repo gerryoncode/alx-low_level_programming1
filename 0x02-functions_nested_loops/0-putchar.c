3349423fac14dd32ce32d517b02190d2f8d76d4e
gerryoncode <gerryoncode@gmail.com>
2023-02-21 09:45:09 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the function
+ * Program prints _putchar followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the README fr the Nested functions Tasks
