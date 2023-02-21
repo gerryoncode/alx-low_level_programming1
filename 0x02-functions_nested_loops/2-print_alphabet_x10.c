5d84b494dd9157706a2529a857cda04a03739a94
gerryoncode <gerryoncode@gmail.com>
2023-02-21 11:03:19 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prits 10 times the lowercae alphabet
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i = 0;
+
+	while (i < 10)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+	}
+}
