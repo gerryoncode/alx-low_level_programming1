9314cfbab91f69ef62b76543d2c8406d4b5a2fb8
gerryoncode <gerryoncode@gmail.com>
2023-02-23 05:27:04 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-int print_numbers(void);
+void print_numbers(void);
 int print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
