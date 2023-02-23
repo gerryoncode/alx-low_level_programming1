664cd257331c7d30d9f91a5ac0817daab596be23
gerryoncode <gerryoncode@gmail.com>
2023-02-23 05:41:39 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,16 +6,15 @@
  *                 only use _putchar twice
  * Return: Always 0 (Success)
 */
-
 void print_most_numbers(void)
 {
-	int num;
-
-	for (num = 0; num <= 9; ++num)
+	int ch;
+	
+	for (ch = 48; ch < 58; ch++)
 	{
-		if (num == 2 || num == 4)
-			continue;
-		_putchar(num + 48);
+		if (ch != 50 && ch != 52)
+			_putchar(ch);
+
 	}
 	_putchar('\n');
 }
