96a57aa6ce2fd4ecd28cc7842ce97524f82449ce
gerryoncode <gerryoncode@gmail.com>
2023-02-23 00:19:36 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  *_isupper - Checks awhether a character is uppercasw or not
+  *@c: actual character under test
+  *Return: 1 if c is uppercase otherwise 0
+  */
+int _isupper(int c)
+{
+	if (c >= 'A' || c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This Readmecontains the solutions to the 0x04 more functions nested loops tasks.
