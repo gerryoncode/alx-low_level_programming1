c58bc4d313d5703781185b915b4ec89be0e8b89c
gerryoncode <gerryoncode@gmail.com>
2023-03-01 08:25:05 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _strncpy - copies strings
- * @dest: First  pointer
- * @src: Second pointer
- * @n: Integer parameter
- * Return: resultant pointer
- */
+*_strncpy - concatenates two strings
+* @dest: string pointer
+* @src: string pointer
+* @n: bytes
+* Return: pointer to the resulting string dest
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; (i < n) && (*(src + i) != '\0'); i++)
 		*(dest + i) = *(src + i);
