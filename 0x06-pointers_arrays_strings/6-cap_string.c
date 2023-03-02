095bf788a97469d0e5e372263a80cfa7180c75d5
gerryoncode <gerryoncode@gmail.com>
2023-03-02 00:48:45 -0800

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+*string_toupper - changes all lowercase letters of a string to uppercase
+*@s: string
+*Return: s
+*/
+/**
+*cap_string - capitalizes all words of a string.
+*@s: string
+*Return: s
+*/
+char *cap_string(char *s)
+{
+	char t[] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
+			 '"', '(', ')', '{', '}'};
+	int i, j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(t + j) != '\0'; j++)
+		{
+			if (i == 0)
+			{
+				if (*(s + i) >= 97 && *(s + i) <= 122)
+					*(s + i) -= 32;
+			}
+			if (*(s + i) == *(t + j))
+			{
+				if (*(s + i + 1) >= 97 && *(s + i + 1) <= 122)
+				{
+					*(s + i + 1) -= 32;
+				}
+			}
+		}
+	}
+	return (s);
+}
