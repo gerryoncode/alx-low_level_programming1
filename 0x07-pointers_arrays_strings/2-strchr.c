0cec036e847493a4974c08071813c6165166ddb3
gerryoncode <gerryoncode@gmail.com>
2023-03-06 09:42:06 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - Returns pointer to first occurence of a character
+ * @s: POinter to a string
+ * @c: Character to be searched
+ * Return: Returns a pointer to found string
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for  (i = 0; s[i] != c; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s);
+		}
+	}
+}
