2bd6af930d0e5a3efb46b50198ffc3fda02a50f0
gerryoncode <gerryoncode@gmail.com>
2023-03-08 09:48:01 -0800

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+int div(int n, int d)
+{
+	if (d *d == n)
+	{
+		return (d);
+	}
+	if (d * d > n)
+	{
+		return (-1);
+	}
+	return (div(n, d + 1));
+}
+#include "main.h"
+/**
+ * _sqrt_recursion: returns the natural squareroot of a number
+ * @n: number to find natural log of 
+ * Return: squareroot of the number or -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	return (div(n, 1);
+}
