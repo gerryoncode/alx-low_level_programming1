89d1c8d02e14c9e6aab87b82e323d9ababcce726
gerryoncode <gerryoncode@gmail.com>
2023-03-14 03:08:53 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 /**
  * free_grid - frees a two dimensional grid
+ * @grid: POinter to the 3D array
+ * @height: height of the array
  * Return: 0
  */
 void free_grid(int **grid, int height)
