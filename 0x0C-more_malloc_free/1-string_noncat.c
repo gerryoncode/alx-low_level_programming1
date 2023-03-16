b05b7a2e133ddfb2ac7b88d7704f0eaa64f6473c
gerryoncode <gerryoncode@gmail.com>
2023-03-16 13:54:37 -0700

    1-string_noncat.c

--- 0x0C-more_malloc_free/1-string_noncat.c
+++ 0x0C-more_malloc_free/1-string_noncat.c
@@ -4,11 +4,40 @@
  * @s1: first string
  * @s2: second string
  * @n: number of bytes of second string
- * Return: pointer to concatenatd s1 and n of s2 or null.
+ * Return: pointer to concatenatd s1 and n of s2 or null
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ar;
-	int l;
+	unsigned int x, y, z;
+	char *s;
 
-	while (s1
+	if (s1 == NULL)
+	{
+		x = 0;
+	}
+	else
+	{
+		for (x = 0; s1[x]; ++x)
+		;
+	}
+	if (s2 == NULL)
+	{
+		y = 0;
+	}
+	else
+	{
+		for (y = 0; s2[y]; ++y)
+		;
+	}
+	if (y > n)
+		y = n;
+	s = malloc(sizeof(char) * (x + y + 1));
+	if (s == NULL)
+		return (NULL);
+	for (z = 0; z < x; z++)
+		s[z] = s1[z];
+	for (z = 0; z < y; z++)
+		s[z + x] = s2[z];
+	s[x + y] = '\0';
+	return (s);
+}
