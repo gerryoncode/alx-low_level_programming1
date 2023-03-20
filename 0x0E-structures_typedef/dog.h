817ff06b95f1b14c14660daccc1614fb5e90e045
gerryoncode <gerryoncode@gmail.com>
2023-03-20 12:25:53 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - represents the dog object
+ * @name: First Member
+ * @age: Second Member
+ * @owner:Third Member
+ *
+ * Description: Represents information of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
