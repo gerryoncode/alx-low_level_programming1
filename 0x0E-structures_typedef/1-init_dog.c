785b668ae0b32a3b7cbabea5b86bf1fca06043ef
gerryoncode <gerryoncode@gmail.com>
2023-03-20 13:02:11 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,10 +1,13 @@
 #include "dog.h"
+
 /**
- * init_dog - initialize a variable of type dog
- * @*d: first parameter
- * @name: Second parameter
- * @age: third parameter
- * @owner: forth parameter
+ * init_dog - initializes a structure of type dog
+ * @d: pointer to structure
+ * @name: pointer to the name of the dog
+ * @age: age of dog
+ * @owner: pointer to owner of dog
+ *
+ * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
@@ -14,4 +17,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d->age = age;
 		d->owner = owner;
 	}
-}	
+}
