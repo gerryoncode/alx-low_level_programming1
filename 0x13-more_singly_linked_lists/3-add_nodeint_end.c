3b559b21aa4b42955e61c5d5f9fca14621f71b3d
gerryoncode <gerryoncode@gmail.com>
2023-04-04 03:52:13 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -17,8 +17,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (NULL);
 	node->n = n;
 	if (node->n == NULL)
+	{
 		free(node);
 		return (NULL);
+	}
 	node->next = NULL;
 	if (*head == NULL)
 	{
