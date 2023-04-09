f00caf68071e913ccfa4ef74ee56792109659964
gerryoncode <gerryoncode@gmail.com>
2023-04-08 21:59:39 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,20 +6,21 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int c;
-	unsigned int j = 0;
+	int i;
+	unsigned int j;
 
+	j = 0;
 	if (!b)
 		return (0);
-	for (c = 0; b[c] == "\0"; c++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[c] != '0' || b[c] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
-	for (c = 0; b[c] != "\0"; c++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		j << 1;
-		if (b[c] == '1')
+		j <<= 1;
+		if (b[i] == '1')
 			j += 1;
 	}
 	return (j);
