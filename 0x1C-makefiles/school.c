3aa6fa03e746348cce38ba83fd04694fca536fae
Gerald <gerryoncode@gmail.com>
2023-07-27 05:47:25 -0700

    2-Makefile

---
+++ 0x1C-makefiles/school.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include "school.h"
+void school_term() {
+	printf("This is the school term");
+}
---
+++ 0x1C-makefiles/school.h
@@ -0,0 +1,6 @@
+#ifndef SCHOOL_H
+#define SCHOOL_H
+
+void school_term();
+
+#endif
