58ab8ee5af61d05d0c33b19bae0c2359c7dfa63d
Gerald <gerryoncode@gmail.com>
2023-09-13 05:40:51 -0700

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,7 +12,7 @@
 int linear_search(int *array, size_t size, int value)
 {
     size_t i;
-    if (array != NULL)
+    if (array != NULL || size > 0)
     {
         for (i = 0; i < size; i++)
         {
